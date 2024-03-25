90631c87326d09daa8a115edb5aeca61d3d0cdee
Cassius de Ávila <cassius_avila@sicredi.com.br>
2024-03-24 22:58:34 -0300

    chore: add pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,49 @@
-import { defineConfig } from 'vite'
-import * as path from 'node:path'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+import * as path from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  VitePWA({
+    registerType: "autoUpdate",
+    manifest: {
+      name: "Schrute",
+      short_name: "Schrute",
+      description: "My Awesome App description",
+      display: "standalone",
+      background_color: "#EBD723",
+      theme_color: "#FFFFFF",
+      icons: [
+        {
+          src: "/android-chrome-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+          purpose: "any",
+        },
+        {
+          src: "/android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "any",
+        },
+        {
+          src: "/android-chrome-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+          purpose: "maskable",
+        },
+        {
+          src: "/android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "maskable",
+        },
+      ],
+    },
+  }),
+  ],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   }
